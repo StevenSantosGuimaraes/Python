47c1625e1a77a1e7a1a3c6c386398a89e3bd8c40
S. S. Guimarães <69175074+StevenSantosGuimaraes@users.noreply.github.com>
2020-11-13 16:55:21 -0300

    Update q02.py

--- Exercises/ex04/q02.py
+++ Exercises/ex04/q02.py
@@ -1,2 +1,9 @@
 # Utiliza a função EPrimo desenvolvida no Exercício resolvido 5.1 para carregar uma lista contendo os N primeiros números primos, em que N é um número inteiro fornecido pelo usuário.
 
+listaPrimos = []
+
+def lPrimos(qTermos):
+
+nTermos = int(input('Informe quantos números primos deseja: '))
+res = listaPrimos(nTermos)
+print(res)
