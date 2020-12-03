787a71a185ad34e5d7f094ac724776507c94d5af
S. S. Guimarães <69175074+StevenSantosGuimaraes@users.noreply.github.com>
2020-12-03 20:45:17 -0300

    Teste

--- Aulas/a_Dicionario.py
+++ Aulas/a_Dicionario.py
@@ -3,3 +3,6 @@ Dicionários são coleções do tipo chave/valor.
 Dicionários são representados por { }.
 OBS: Em algumas linguagens de programação, os dicionários Python são conhecidos por mapas.
 """
+paises = {'br': 'Brasil', 'eua': 'Estados Unidos da América', 'py': 'Paraguai'}
+print(paises)
+print(type(paises))
