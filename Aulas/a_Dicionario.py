7be40502529b03d3379758b39c3f842a55e8cec8
S. S. Guimarães <69175074+StevenSantosGuimaraes@users.noreply.github.com>
2020-12-03 20:43:00 -0300

    Teste

--- Aulas/a_Dicionario.py
+++ Aulas/a_Dicionario.py
@@ -1,4 +1,5 @@
 """
 Dicionários são coleções do tipo chave/valor.
+Dicionários são representados por { }.
 OBS: Em algumas linguagens de programação, os dicionários Python são conhecidos por mapas.
 """
