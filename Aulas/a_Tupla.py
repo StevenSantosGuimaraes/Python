9604f48434144e0e6d6cd7e66c51fb579b307c5d
S. S. Guimarães <69175074+StevenSantosGuimaraes@users.noreply.github.com>
2020-12-03 20:39:48 -0300

    Teste

--- Aulas/a_Tupla.py
+++ Aulas/a_Tupla.py
@@ -5,7 +5,3 @@ Diferenças:
 - As tuplas são imutáveis: logo ao se criar uma tupla ela não muda. Toda operação em uma tupla gera uma nova tupla.
 
 """
-tupla = ('Geek University', 'programção em Python: Essencial')
-escola, curso = tupla
-print(escola)
-print(curso)
