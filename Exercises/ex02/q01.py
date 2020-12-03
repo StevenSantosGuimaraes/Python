f8d8526627345fc8d43fa53f689a932c5c9172b9
S. S. Guimarães <69175074+StevenSantosGuimaraes@users.noreply.github.com>
2020-12-03 15:15:43 -0300

    test

--- Exercises/ex02/q01.py
+++ Exercises/ex02/q01.py
@@ -2,4 +2,18 @@
 Lista em Python funcionam como vetores/matrizes (arrays) em outras linguagens, com a diferença de serem DINÂMICO
 e também podemos colocar QUALQUER tipo de dado.
 """
-
+lista1 = list(range(10))
+print(lista1)
+lista2 = list(range(3))
+print(lista2)
+lista1.append(lista2)
+print(lista1)
+lista1.extend(lista2)
+print(lista1)
+print(lista1[-4][-1])
+lista1.pop(-2)
+print(lista1)
+lista3 = ['Programação', 'em', 'Python']
+print(lista3)
+curso = ' '.join(lista3)
+print(curso)
