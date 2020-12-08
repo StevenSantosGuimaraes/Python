84f397eaed3b7751e35248c8ec375541983eb671
S. S. Guimarães <69175074+StevenSantosGuimaraes@users.noreply.github.com>
2020-12-08 10:35:05 -0300

    Ajuste, questão 1.13

--- Exercises/ex01/q13.py
+++ Exercises/ex01/q13.py
@@ -1,4 +1,4 @@
 num1 = int(input("Informe o primeiro número inteiro: "))
 num2 = int(input("Informe o segundo número inteiro: "))
 num3 = num1 + num2
-print(f"Os números {num1} e {num2} somados corresponde a {num3}")
+print(f"Os números {num1} e {num2} somados corresponde a {num3}.")
