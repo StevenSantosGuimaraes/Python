03d6a928b1f17b4429c735ad277fc6c84cba26b0
S. S. Guimarães <69175074+StevenSantosGuimaraes@users.noreply.github.com>
2020-12-08 10:51:09 -0300

    Ajuste, questão 1.15

--- Exercises/ex01/q15.py
+++ Exercises/ex01/q15.py
@@ -1,1 +1,10 @@
-
+num1 = int(input("Informe o primeiro número inteiro: "))
+num2 = int(input("Informe o segundo número inteiro: "))
+num_mult = num1 * num2
+num_div = num1 / num2
+num_soma = num1 + num2
+num_sub = num1 - num2
+print(f"Produto de {num1} e {num2} corresponde a {num_mult}")
+print(f"Divisão de {num1} por {num2} corresponde a {num_div}")
+print(f"Soma de {num1} e {num2} corresponde a {num_soma}")
+print(f"Subtração de {num1} por {num2} corresponde a {num_sub}")
