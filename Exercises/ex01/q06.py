4a4d8d9a97c0f1f8c7ba0b7fb38d4e1427776c2f
S. S. Guimarães <69175074+StevenSantosGuimaraes@users.noreply.github.com>
2020-12-08 09:15:33 -0300

    Ajuste, questão 1.06

---
+++ Exercises/ex01/q06.py
@@ -0,0 +1,3 @@
+temp_celsius = float(input("Olá, informe a temperatura em graus Celsius: "))
+temp_farenheit = temp_celsius * 1.8 + 32
+print(f"A temperatura correspontente em a {temp_celsius} em Farenheit é: {temp_farenheit}")
