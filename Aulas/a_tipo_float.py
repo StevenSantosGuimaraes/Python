bd872414fc8ae259af995de40bf6a85d8b448ab6
S. S. Guimarães <69175074+StevenSantosGuimaraes@users.noreply.github.com>
2020-12-07 21:54:57 -0300

    Update a_tipo_float.py

--- Aulas/a_tipo_float.py
+++ Aulas/a_tipo_float.py
@@ -3,5 +3,10 @@ Tipo Float:
 
 - Tipo Real;
 - Tipo Decimal;
-- Contém casas decimais...
+
+Logo o número contém casas decimais.
+===> OBS: O separador de casas decimais na programação é o " . " (Ponto) e não a " , " (Virgula).
+
+Ex (Errado): 3,14
+Ex (Correto): 3.14
 """
