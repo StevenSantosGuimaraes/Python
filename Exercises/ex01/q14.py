5030a41aae9b693f114e6a6d8784e18c3281022f
S. S. Guimarães <69175074+StevenSantosGuimaraes@users.noreply.github.com>
2020-12-08 10:47:18 -0300

    Ajuste, questão 1.14

---
+++ Exercises/ex01/q14.py
@@ -0,0 +1,4 @@
+num1 = int(input("Informe o primeiro número inteiro: "))
+num2 = int(input("Informe o segundo número inteiro: "))
+num3 = num1 * num2
+print(f"O produto dos números {num1} e {num2} corresponde a {num3}")
