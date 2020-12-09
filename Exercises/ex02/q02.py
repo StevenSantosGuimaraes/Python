1557f8691ee879becc7d048bef2d27012da4ca34
S. S. Guimarães <69175074+StevenSantosGuimaraes@users.noreply.github.com>
2020-12-08 21:12:19 -0300

    Atualização 2.02

--- Exercises/ex02/q02.py
+++ Exercises/ex02/q02.py
@@ -1,3 +1,8 @@
 num = int(input("Informe um número: "))
 
-if num
+if num > 10:
+    print("O número informado é maior que 10 !!!")
+elif num == 10:
+    print("O número informado é igual a 10 !!!")
+else:
+    print("O número informado é menor que 10 !!!")
