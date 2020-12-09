39ae0cf07b2feb5bb5d514c0e9909cc7fad9f510
S. S. Guimarães <69175074+StevenSantosGuimaraes@users.noreply.github.com>
2020-12-08 21:16:21 -0300

    Atualização 2.02

---
+++ Exercises/ex02/q04.py
@@ -0,0 +1,9 @@
+num = int(input("Informe um número: "))
+
+if num > 0:
+    print("O número informado é positivo !!!")
+elif num < 0:
+    print("O número informado é negativo !!!")
+else:
+    print("O número informado é nulo !!!")
+
