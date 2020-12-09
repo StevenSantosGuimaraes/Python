8f2c08407dadcf2c30f6e325a1c3a1253910dc96
S. S. Guimarães <69175074+StevenSantosGuimaraes@users.noreply.github.com>
2020-12-08 21:17:59 -0300

    Atualização 2.05

---
+++ Exercises/ex02/q05.py
@@ -0,0 +1,8 @@
+num = int(input("Informe um número: "))
+teste = num // 3
+
+if teste == 0:
+    print("O número informado é multiplo de 3 !!!")
+else:
+    print("O número informado não é multiplo de 3 !!!")
+
