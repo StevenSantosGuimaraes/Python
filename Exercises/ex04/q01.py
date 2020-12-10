1194cccc9b9d1f5382073ba164629e403d3bfb27
S. S. Guimarães <69175074+StevenSantosGuimaraes@users.noreply.github.com>
2020-12-10 08:44:38 -0300

    Atualização: 4.01

--- Exercises/ex04/q01.py
+++ Exercises/ex04/q01.py
@@ -1,13 +1,12 @@
-# Escreva uma função que recebe um número inteiro como parãmetro de entrada e retona o texto "PAR" ou "ÍMPAR".
-
-def ParImpar(num):
-    if num % 2 == 1:
-        msg = "IMPAR."
+def par_impar(num):
+    if num % 2 == 0:
+        msg = "PAR!"
         return msg
     else:
-        msg = "PAR."
+        msg = "ÍMPAR!"
         return msg
 
-X = int(input("Informe um número: "))
-res = ParImpar(X)
-print("O número informado foi {0} e ele é: {1} !!!".format(X, res))
+
+x = int(input("Informe um número: "))
+res = par_impar(x)
+print(f'O número informado foi {x} e ele é: {res}')
