beffa9cc17df477a5369b26991731f9a6a040328
S. S. Guimarães <69175074+StevenSantosGuimaraes@users.noreply.github.com>
2020-12-11 12:43:28 -0300

    Atualização: 4.11

--- livro/c4q10.py
+++ livro/c4q10.py
@@ -1,1 +1,11 @@
+lista = []
+res = 'n'
+i, num = 0, 0
+while res != 's' or res != 'S':
+    num = int(input(f'Informe o {i + 1}° valor: '))
+    lista.append(num)
+    res = str(input('Deseja sair? [Sim/Não] '))
+    if res == 'n' or 'N':
+        i += 1
 
+print(lista)
